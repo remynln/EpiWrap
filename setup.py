5655df3668870e13f802b8ae83dda7989179df62
Waz0x <remy.noulin@epitech.eu>
2021-08-16 13:19:48 +0200

    MOD | setup

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     url="https://github.com/Waz0x/EpiWrap",
     download_url="https://github.com/Waz0x/EpiWrap/archive/refs/tags/v0.1.tar.gz",
     keywords=["epitech", "doc", "intranet"],
-    classifiers= []
+    classifiers=[]
 )
