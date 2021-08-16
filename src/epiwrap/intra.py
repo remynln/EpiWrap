ea15e126262319189583231d0bba5044c356c4a6
Waz0x <remy.noulin@epitech.eu>
2021-08-16 13:01:54 +0200

    ADD | user wrapper

--- src/epiwrap/intra.py
+++ src/epiwrap/intra.py
@@ -1,73 +1,58 @@
-from flask import Blueprint, request
-import json
-from time import strftime
-from datetime import timedelta, date, datetime
-from src import formater as f
+import requests as r
 
 
-class URLArgError(Exception):
-
-    def __init__(self, value):
-        self.message = value
-        self.value = "URLArg Error : " + self.message
+class EpiError(Exception):
+    def __init__(self, err):
+        self.message = err
+        self.value = "EpiError Error : " + self.message
 
     def __str__(self):
         return repr(self.value)
 
 
-def getArg(key, default=None):
-    args = request.args.get(key)
-    head = request.headers.get(key)
-    if not args and not head:
-        if default is not None:
-            return default
-        else:
-            raise URLArgError("Missing argument {0}".format(key))
-    return args if args else head
+class Student(object):
+    """coucou"""
+    def __init__(self, token, header):
+        try:
+            res = r.get("https://intra.epitech.eu/user/?format=json", headers=header, cookies=token).json()
+            self.fullname = res["title"]
+            self.gpa = res["gpa"][0]["gpa"]
+            self.firstname = res["firstname"]
+            self.lastname = res["lastname"]
+            self.city = res["groups"][0]["title"]
+            self.credits = res["credits"]
+            self.scolaryear = res["promo"]
+            self.semester = res["semester"]
+        except Exception as e:
+            print(e)
 
 
-def checkAuth(req):
-    if not req:
-        return f.error("Missing token", 401)
-    result = f.executeRequest(req)
-    if result["code"] == 200:
-        return f.response(result["data"], 200)
-    if "message" in result["data"]:
-        return f.error(result["data"]["message"], 401)
-    elif "error" in result["data"]:
-        return f.error(result["data"]["error"], 401)
-    else:
-        return f.error("There is an error, try again", 401)
+class EpiWrap(object):
+    """Simple local wrapper for epitech intra"""
 
+    def __init__(self, token):
+        """Enter token in parameters to initiate the client"""
+        self._done = False
+        self._url = ""
+        self._types = {"GET": r.get,
+                       "POST": r.post,
+                       "DELETE": r.delete}
+        self._method = ""
+        self._data = "bytearray(json.dumps(data), 'utf8')"
+        self._error = None
+        self._request = None
+        self._token = {"user": token}
+        self._headers = {"Content-Type": "application/json", "Accept-Language": "fr,en-US;q=0.8,en;q=0.6"}
 
-def getTokenRequest(url, method, data=None):
-    req = f.Request(url, method, data)
-    try:
-        req.setCookie("user", getArg("token"))
-        return req
-    except URLArgError:
-        return None
+    def getToken(self):
+        return self._token
 
+    def getHeader(self):
+        return self._headers
 
-def get_route(route):
-    return "https://intra.epitech.eu" + route
-
-
-def root():
-    return f.response({"status": True}, 200)
-
-
-def getInfos():
-    req = getTokenRequest(get_route("/?format=json"), "GET")
-    return checkAuth(req)
-
-
-def getPlanning():
-    try:
-        start = getArg("start", strftime("%Y-%m-%d", date.today().timetuple()))
-        end = getArg("end", (datetime.strptime(start, "%Y-%m-%d") + timedelta(days=6)))
-        route = "/intra/planning/load?format=json&start={0}&end={1}".format(start, str(end).split(' ')[0])
-    except URLArgError as e:
-        return f.error(e.message, 401)
-    req = getTokenRequest(get_route(route), "POST")
-    return checkAuth(req)
+    def getProfile(self):
+        try:
+            student = Student(self.getToken(), self.getHeader())
+            return student
+        except EpiError as e:
+            print(e)
