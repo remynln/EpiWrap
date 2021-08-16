bb67aa81de72d1a20b9bb464a00177afea4983e2
Waz0x <remy.noulin@epitech.eu>
2021-08-16 13:18:35 +0200

    ADD | package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="EpiWrap",
+    packages=['EpiWrap'],
+    version='0.1',
+    description="Wrapper for epitech intranet",
+    author="Waz0x",
+    author_email="remy.noulin@epitech.eu",
+    url="https://github.com/Waz0x/EpiWrap",
+    download_url="https://github.com/Waz0x/EpiWrap/archive/refs/tags/v0.1.tar.gz",
+    keywords=["epitech", "doc", "intranet"],
+    classifiers= []
+)
